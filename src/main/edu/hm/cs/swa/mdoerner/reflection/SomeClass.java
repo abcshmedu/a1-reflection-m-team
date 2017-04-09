25a707e75cfb6852b8a0882c80dfd6cd615dce0a
Martin Doerner <martin.doerner@yahoo.de>
2017-04-09 16:38:09 +0200

    Checkstyle fixed, simple parameterized test included

--- src/main/edu/hm/cs/swa/mdoerner/reflection/SomeClass.java
+++ src/main/edu/hm/cs/swa/mdoerner/reflection/SomeClass.java
@@ -1,59 +1,30 @@
 package edu.hm.cs.swa.mdoerner.reflection;
 
-import java.util.*;
+import java.util.Date;
 
 /**
+ * Simple class to check if the Renderer is working.
  * 
- * @author Martin Doerner based on the example given
+ * @author Example given - SWA SomSem17
  *
  */
 public class SomeClass {
     @RenderMe
     private int foo;
     @RenderMe(with = "edu.hm.cs.swa.mdoerner.reflection.ArrayRenderer")
-    int[] array = { 1, 2, 3, };
-    @RenderMe
-    private Date date = new Date(123456789);
+    private final int[] array = { 1, 2, 3, };
     
+    @RenderMe
+    private final Date date = new Date(123456789);
 
+    /**
+     * Constructor with an integer as parameter to render later.
+     * 
+     * @param foo
+     *            - Integer that is saved in this object and should be rendered.
+     */
     public SomeClass(int foo) {
         this.foo = foo;
     }
-    
-    public SomeClass(String[] array){
-        
-    }
 
-    @RenderMe
-    public int someInt() {
-        return 1;
-    }
-
-    @RenderMe
-    public void someVoid() {
-    }
-
-    @RenderMe
-    public int[] someIntArray() {
-        int[] res = { 3, 1, 4 };
-        return res;
-    }
-
-    @RenderMe(with = "edu.hm.cs.swa.mdoerner.reflection.ArrayRenderer")
-    public int[] someIntArrayRen() {
-        int[] res = { 4, 5, 6 };
-        return res;
-    }
-
-    @RenderMe
-    public String[] someStringArray(){
-        String[] res = {"hm", "edu"};
-        return res;
-    }
-    
-    @RenderMe(with = "edu.hm.cs.swa.mdoerner.reflection.ArrayRenderer")
-    public String[] someStringArrayRen(){
-        String[] res = {"truck", "bus"};
-        return res;
-    }
 }
--- src/main/edu/hm/cs/swa/mdoerner/reflection/test/RendererTest.java
+++ src/main/edu/hm/cs/swa/mdoerner/reflection/test/RendererTest.java
@@ -2,7 +2,9 @@ package edu.hm.cs.swa.mdoerner.reflection.test;
 
 import static org.junit.Assert.assertEquals;
 
-import org.junit.Before;
+import java.util.Arrays;
+import java.util.Collection;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
@@ -11,22 +13,54 @@ import org.junit.runners.Parameterized.Parameters;
 import edu.hm.cs.swa.mdoerner.reflection.Renderer;
 import edu.hm.cs.swa.mdoerner.reflection.SomeClass;
 
+/**
+ * Test class for parameterized test of SomeClass.
+ * 
+ * @author Martin Doerner
+ *
+ */
 @RunWith(Parameterized.class)
 public class RendererTest {
     private SomeClass toRender;
     private Renderer renderer;
 
-    @Before
-    public void setUp() {
-        toRender = new SomeClass(5);
+    /**
+     * Parameters for the SomeClass test - input and expected output values.
+     * 
+     * @return List of input and expected output objects.
+     */
+    @Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][] {
+                { 0, "Instance of edu.hm.cs.swa.mdoerner.reflection.SomeClass:\nfoo (Type int): 0\narray (Type int[]) [1, 2, 3, ]\ndate (Type java.util.Date): Fri Jan 02 11:17:36 CET 1970\n" },
+                { 1, "Instance of edu.hm.cs.swa.mdoerner.reflection.SomeClass:\nfoo (Type int): 1\narray (Type int[]) [1, 2, 3, ]\ndate (Type java.util.Date): Fri Jan 02 11:17:36 CET 1970\n" },
+                { 2, "Instance of edu.hm.cs.swa.mdoerner.reflection.SomeClass:\nfoo (Type int): 2\narray (Type int[]) [1, 2, 3, ]\ndate (Type java.util.Date): Fri Jan 02 11:17:36 CET 1970\n" } });
+    }
+
+    private int inputNumber;
+    private String output;
+
+    /**
+     * 
+     * @param input
+     *            - Value for foo-int in SomeClass.
+     * @param expected
+     *            - String expected as output.
+     */
+    public RendererTest(int input, String expected) {
+        this.inputNumber = input;
+        this.output = expected;
+        toRender = new SomeClass(inputNumber);
         renderer = new Renderer(toRender);
     }
 
+    /**
+     * Tests SomeClass using the parameters mapped to input int and output
+     * String.
+     */
     @Test
-    public void testRendering() throws Exception {
-        assertEquals(
-                "Instance of edu.hm.cs.swa.mdoerner.reflection.SomeClass:\n"
-                        + "foo (Type int): 5\narray (Type int[]) [1, 2, 3, ]\ndate (Type java.util.Date): Fri Jan 02 11:17:36 CET 1970\n",
-                renderer.render());
+    public void testRenderer() {
+        String result = renderer.render();
+        assertEquals(this.output, result);
     }
 }
